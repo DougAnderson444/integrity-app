1def5d96e99c7d73fc6f9cc5ebff7d232e1a2539
Doug Anderson444 <douganderson444@gmail.com>
2024-03-29 00:07:08 -0300

    rm dead

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import fs from 'fs';
 
 const index = fs.readFileSync('./wallet/dist/index.html', 'utf-8');
 const template = fs.readFileSync('./wallet/template.js', 'utf-8');
-const encoded = btoa(index);
 const wallet = template.replace('%%index%%', index.replace(/`/g, '\\`').replace(/\$/g, '\\$'));
 
 fs.writeFileSync('static/wallet.js', wallet);
