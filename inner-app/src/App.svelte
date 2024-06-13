f85208ffe1010f10d4dd48acb543c0366bd560ba
Doug Anderson444 <douganderson444@gmail.com>
2024-06-13 14:28:49 -0300

    use `wurbo` v0.4.1 with inline worker format

--- inner-app/src/App.svelte
+++ inner-app/src/App.svelte
@@ -1,7 +1,13 @@
 <script>
+	import { onMount } from 'svelte';
 	import { WurboLoader } from 'wurbo';
+
+	onMount(() => {
+		// TODO: Hardcode the app wasm default?
+		// TODO: window.opener API for signing and data exchange (backup, export, etc.)
+	});
 </script>
 
 <main>
-	<WurboLoader />
+	<WurboLoader inline={true} />
 </main>
